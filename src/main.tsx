592479ca1623636bd8fdb44a890254594b8c06e6
András Rakita <Rakita.Andras@gmail.com>
2024-09-27 20:46:23 +0200

    chore: adding react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,27 @@
-import { AppThemeProvider } from './themes/AppThemeProvider';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Analytics } from '@vercel/analytics/react';
 import { Provider } from 'react-redux';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { QueryClientProvider } from '@tanstack/react-query';
+
 import store from './app/store';
-import React from 'react';
+import queryClient from './app/client';
+import { AppThemeProvider } from './themes/AppThemeProvider';
+
 import App from './App';
 import './main.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <AppThemeProvider>
-        <App />
-        <Analytics />
-      </AppThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <AppThemeProvider>
+          <App />
+          <Analytics />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </AppThemeProvider>
+      </QueryClientProvider>
     </Provider>
   </React.StrictMode>,
 );
