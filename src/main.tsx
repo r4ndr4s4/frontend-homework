9d18134c1cc24df382340ae1b2ae155dcc9f7389
András Rakita <Rakita.Andras@gmail.com>
2024-09-27 20:25:06 +0200

    feat: adding analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { AppThemeProvider } from './themes/AppThemeProvider';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/react';
 import { Provider } from 'react-redux';
 import store from './app/store';
 import React from 'react';
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <Provider store={store}>
       <AppThemeProvider>
         <App />
+        <Analytics />
       </AppThemeProvider>
     </Provider>
   </React.StrictMode>,
