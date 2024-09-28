c936b5245ef7343f877fd00d92b9b3237828d559
András Rakita <Rakita.Andras@gmail.com>
2024-09-28 23:27:41 +0200

    chore: simplifying favorites

--- src/components/Breeds.tsx
+++ src/components/Breeds.tsx
@@ -1,50 +1,50 @@
 import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
 import { IconButton } from '@mui/material';
 import LaunchIcon from '@mui/icons-material/Launch';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import dogs from '@/assets/dogs.json';
 
-export const rows: GridRowsProp = dogs.map((dog) => ({
-  id: dog.id,
-  breed: 'https://placehold.co/50x50', // dog.reference_image_id
-  name: dog.name,
-  group: dog.breed_group,
-  life: dog.life_span,
-}));
+function Breeds({ columns }: { columns?: GridColDef[] }) {
+  const navigate = useNavigate();
 
-const columns: GridColDef[] = [
-  {
-    field: 'breed',
-    headerName: 'Breed',
-    width: 70,
-    sortable: false,
-    filterable: false,
-    disableColumnMenu: true,
-    renderCell: (params) => <img src={params.value} />,
-  },
-  { field: 'name', headerName: 'Breed name', width: 300 },
-  { field: 'group', headerName: 'Breed group', width: 200, sortable: false },
-  { field: 'life', headerName: 'Life expectancy', width: 200, filterable: false, disableColumnMenu: true },
-  {
-    field: 'open',
-    headerName: '',
-    width: 70,
-    sortable: false,
-    filterable: false,
-    disableColumnMenu: true,
-    renderCell: (e) => (
-      <Link to={`/breeds/${e.row.id}`}>
-        <IconButton aria-label="go to breed">
+  const rows: GridRowsProp = dogs.map((dog) => ({
+    id: dog.id,
+    breed: 'https://placehold.co/50x50', // dog.reference_image_id
+    name: dog.name,
+    group: dog.breed_group,
+    life: dog.life_span,
+  }));
+
+  const defaultColumns: GridColDef[] = [
+    {
+      field: 'breed',
+      headerName: 'Breed',
+      width: 70,
+      sortable: false,
+      filterable: false,
+      disableColumnMenu: true,
+      renderCell: (params) => <img src={params.value} />,
+    },
+    { field: 'name', headerName: 'Breed name', width: 300 },
+    { field: 'group', headerName: 'Breed group', width: 200, sortable: false },
+    { field: 'life', headerName: 'Life expectancy', width: 200, filterable: false, disableColumnMenu: true },
+    {
+      field: 'open',
+      headerName: '',
+      width: 70,
+      sortable: false,
+      filterable: false,
+      disableColumnMenu: true,
+      renderCell: (e) => (
+        <IconButton aria-label="go to breed" onClick={() => navigate(`/breeds/${e.row.id}`)}>
           <LaunchIcon />
         </IconButton>
-      </Link>
-    ),
-  },
-];
+      ),
+    },
+  ];
 
-function Breeds() {
-  return <DataGrid rows={rows} columns={columns} disableRowSelectionOnClick disableColumnResize />;
+  return <DataGrid rows={rows} columns={columns || defaultColumns} disableRowSelectionOnClick disableColumnResize />;
 }
 
 export default Breeds;
--- src/components/Favorites.tsx
+++ src/components/Favorites.tsx
@@ -1,39 +1,39 @@
-import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { GridColDef } from '@mui/x-data-grid';
 import IconButton from '@mui/material/IconButton';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 
-import { rows } from './Breeds';
-
-const columns: GridColDef[] = [
-  {
-    field: 'breed',
-    headerName: 'Breed',
-    width: 70,
-    sortable: false,
-    filterable: false,
-    disableColumnMenu: true,
-    renderCell: (params) => <img src={params.value} />,
-  },
-  { field: 'name', headerName: 'Breed name', width: 300 },
-  { field: 'group', headerName: 'Breed group', width: 200, sortable: false },
-  { field: 'life', headerName: 'Life expectancy', width: 200, filterable: false, disableColumnMenu: true },
-  {
-    field: 'favorite',
-    headerName: '',
-    width: 70,
-    sortable: false,
-    filterable: false,
-    disableColumnMenu: true,
-    renderCell: () => (
-      <IconButton aria-label="add to favorites">
-        <FavoriteIcon />
-      </IconButton>
-    ),
-  },
-];
+import Breeds from './Breeds';
 
 function Favorites() {
-  return <DataGrid rows={rows} columns={columns} disableRowSelectionOnClick />;
+  const columns: GridColDef[] = [
+    {
+      field: 'breed',
+      headerName: 'Breed',
+      width: 70,
+      sortable: false,
+      filterable: false,
+      disableColumnMenu: true,
+      renderCell: (params) => <img src={params.value} />,
+    },
+    { field: 'name', headerName: 'Breed name', width: 300 },
+    { field: 'group', headerName: 'Breed group', width: 200, sortable: false },
+    { field: 'life', headerName: 'Life expectancy', width: 200, filterable: false, disableColumnMenu: true },
+    {
+      field: 'favorite',
+      headerName: '',
+      width: 70,
+      sortable: false,
+      filterable: false,
+      disableColumnMenu: true,
+      renderCell: () => (
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+      ),
+    },
+  ];
+
+  return <Breeds columns={columns} />;
 }
 
 export default Favorites;
