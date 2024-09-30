26ae5f77cee3685c3a23a1f50d84dbb9af9443d2
András Rakita <Rakita.Andras@gmail.com>
2024-09-30 05:32:21 +0200

    feat: persisting favorites

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { BrowserRouter } from 'react-router-dom';
 import { CssBaseline } from '@mui/material';
+import { useEffect } from 'react';
 
 import Routing from './routes/Routing';
 import Menu from './components/Menu';
+import { useAppDispatch } from './app/store';
+import { restore as restoreFavorites } from './features/favoritesSlice';
 
 function App() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(restoreFavorites());
+  }, []);
+
   return (
     <BrowserRouter>
       <CssBaseline />
--- src/features/favoritesSlice.ts
+++ src/features/favoritesSlice.ts
@@ -12,19 +12,28 @@ const favoritesSlice = createSlice({
   name: 'favorites',
   initialState,
   reducers: {
+    restore: (state) => {
+      const favorites = JSON.parse(localStorage.getItem('favorites') || '[]');
+
+      state.breedIds = favorites;
+    },
     add: (state, action: PayloadAction<number>) => {
       const breed = state.breedIds.find((breedId) => breedId === action.payload);
 
       if (!breed) {
         state.breedIds.push(action.payload);
       }
+
+      localStorage.setItem('favorites', JSON.stringify(state.breedIds));
     },
     remove: (state, action: PayloadAction<number>) => {
       state.breedIds = state.breedIds.filter((breedId) => breedId !== action.payload);
+
+      localStorage.setItem('favorites', JSON.stringify(state.breedIds));
     },
   },
 });
 
 export const favoritesReducer = favoritesSlice.reducer;
 
-export const { add, remove } = favoritesSlice.actions;
+export const { restore, add, remove } = favoritesSlice.actions;
