71fa66feb5fc7178912119ace242484ee6d63a71
András Rakita <Rakita.Andras@gmail.com>
2024-10-01 05:17:22 +0200

    chore: texts on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,13 @@ import Container from '@/components/Container';
 function HomePage() {
   return (
     <Container>
-      <Typography variant="h1">Hello World!</Typography>
+      <Typography variant="h1" gutterBottom>
+        Hello World!
+      </Typography>
+
+      <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+        Checkout the breeds loaded from The Dog API, add breeds to favorite and take a quiz to test your knowledge.
+      </Typography>
     </Container>
   );
 }
